d508eaf33378be66fec00380e32710eae1498084
zebpaa <skvorcov_90@mail.ru>
2024-06-25 22:10:11 +0400

    added arguments and option - format

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,6 +3,8 @@ import { program } from 'commander';
 
 program
   .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1>, <filepath2>')
   .helpOption('-h, --help', 'output usage information')
   .version('0.0.1')
+  .option('-f, --format [type]', 'output format')
   .parse();
