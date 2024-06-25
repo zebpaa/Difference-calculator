a1ce5fd31daae6db2b37621ed2f746c43c6eaf40
zebpaa <skvorcov_90@mail.ru>
2024-06-26 00:22:23 +0400

    imported gendiff function and added action with gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import { program } from 'commander';
+import gendiff from '../index.js';
 
 program
   .description('Compares two configuration files and shows a difference.')
@@ -7,4 +8,7 @@ program
   .helpOption('-h, --help', 'output usage information')
   .version('0.0.1')
   .option('-f, --format [type]', 'output format')
+  .action((path1, path2) => {
+    gendiff(path1, path2);
+  })
   .parse();
