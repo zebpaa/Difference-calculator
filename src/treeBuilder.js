10aa3aaec34efad84448c62f04e7f4aab767f138
zebpaa <skvorcov_90@mail.ru>
2024-06-27 18:41:35 +0400

    fixed buildTree function

--- src/treeBuilder.js
+++ src/treeBuilder.js
@@ -1,24 +1,27 @@
 import _ from 'lodash';
 
 const buildTree = (data1, data2) => {
-  const keys = _.union(Object.keys(data1), Object.keys(data2));
-  const sortedKeys = _.sortBy(keys);
-  const result = {};
+  const arr = _.union(Object.keys(data1), Object.keys(data2));
+  const sortedKeys = _.sortBy(arr);
 
-  for (const key in sortedKeys) {
-    if (!Object.hasOwn(data1, key)) {
-      result[`+ ${key}`] = data2[key];
-    } else if (!Object.hasOwn(data2, key)) {
-      result[`- ${key}`] = data1[key];
-    } else if (data1[key] !== data2[key]) {
-      result[`- ${key}`] = data1[key];
-      result[`+ ${key}`] = data2[key];
-    } else {
-      result[`  ${key}`] = data1[key];
+  return sortedKeys.map((key) => {
+    if (_.isPlainObject(data1[key] && _.isPlainObject(data2[key]))) {
+      return { key, type: 'nested', children: buildTree(data1[key], data2[key]) };
     }
-  }
+    if (!Object.hasOwn(data1, key)) return { key, value: data2[key], type: 'added' };
 
-  return JSON.stringify(result);
+    if (!Object.hasOwn(data2, key)) return { key, value: data1[key], type: 'deleted' };
+
+    if (data1[key] !== data2[key]) {
+      return {
+        key,
+        value1: data1[key],
+        value2: data2[key],
+        type: 'changed',
+      };
+    }
+    return { key, value: data1[key], type: 'unchanged' };
+  });
 };
 
 export default buildTree;
