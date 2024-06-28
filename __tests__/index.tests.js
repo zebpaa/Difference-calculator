584c739c83351d73218f35526bde34c90094655f
zebpaa <skvorcov_90@mail.ru>
2024-06-28 10:29:59 +0400

    fixed test file

--- __tests__/index.tests.js
+++ __tests__/index.tests.js
@@ -1,29 +1,19 @@
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+import fs from 'fs';
 import gendiff from '../index.js';
-// import { fileURLToPath } from 'url';
-// import { dirname } from 'path';
-// import path from 'path';
 
-// test('gendiff', () => {
-//   //   const __filename = fileURLToPath(import.meta.url);
-//   //   const __dirname = dirname(__filename);
-
-// const result = {
-//   '- follow': false,
-//   '  host': 'hexlet.io',
-//   '- proxy': '123.234.53.22',
-//   '- timeout': 50,
-//   '+ timeout': 20,
-//   '+ verbose': true,
-// };
-
-//   //   const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-//   expect(gendiff(filepath1, filepath2)).toBe(result);
-// });
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 test('gendiff', () => {
-  const filepath1 = '/home/zebpaa/frontend-project-46/__fixtures__/file1.json';
-  const filepath2 = '__fixtures__/file2.json';
-  const result = {
+  readFile('file1.json');
+  readFile('file2.json');
+  const filepath1 = getFixturePath('file1.json');
+  const filepath2 = getFixturePath('file2.json');
+  const json = {
     '- follow': false,
     '  host': 'hexlet.io',
     '- proxy': '123.234.53.22',
@@ -31,5 +21,6 @@ test('gendiff', () => {
     '+ timeout': 20,
     '+ verbose': true,
   };
+  const result = JSON.stringify(json, null, 2);
   expect(gendiff(filepath1, filepath2)).toEqual(result);
 });
