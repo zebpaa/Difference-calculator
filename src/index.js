85d575013afefb2558c3c0c941417f98439d61e7
zebpaa <skvorcov_90@mail.ru>
2024-06-30 20:37:53 +0400

    added default option formatName

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import path from 'path';
 import fs from 'fs';
 import parse from './parsers.js';
 import buildTree from './treeBuilder.js';
-import format from './formatters/stylish.js';
+import format from './formatters/index.js';
 
 const fullFilePath = (filepath) => path.resolve(process.cwd(), filepath);
 const extractFormat = (filepath) => path.extname(filepath).slice(1);
 const getData = (filepath) => parse(fs.readFileSync(filepath, 'utf-8'), extractFormat(filepath));
 
-const gendiff = (filepath1, filepath2, formatName) => {
+const gendiff = (filepath1, filepath2, formatName = 'stylish') => {
   const fullFilePath1 = fullFilePath(filepath1);
   const fullFilePath2 = fullFilePath(filepath2);
   const data1 = getData(fullFilePath1);
