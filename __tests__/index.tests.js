b1e716e64ddfcb695e99ff3f14b4eb0878427bbb
zebpaa <skvorcov_90@mail.ru>
2024-06-30 14:52:38 +0400

    fixed 1st test and added 2nd test, but commented him

--- __tests__/index.tests.js
+++ __tests__/index.tests.js
@@ -1,26 +1,79 @@
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
-import fs from 'fs';
+// import fs from 'fs';
 import gendiff from '../index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+// const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test('gendiff', () => {
-  readFile('file1.json');
-  readFile('file2.json');
-  const filepath1 = getFixturePath('file1.json');
-  const filepath2 = getFixturePath('file2.json');
-  const json = {
-    '- follow': false,
-    '  host': 'hexlet.io',
-    '- proxy': '123.234.53.22',
-    '- timeout': 50,
-    '+ timeout': 20,
-    '+ verbose': true,
-  };
-  const result = JSON.stringify(json, null, 2);
+test('Сравнение плоских файлов (yaml)', () => {
+  const filepath1 = getFixturePath('file1.yaml');
+  const filepath2 = getFixturePath('file2.yml');
+  // readFile('file1.yaml');
+  // readFile('file2.yml');
+  const result = `{
+ - follow: false
+   host: hexlet.io
+ - proxy: 123.234.53.22
+ - timeout: 50
+ + timeout: 20
+ + verbose: true
+}`;
+
   expect(gendiff(filepath1, filepath2)).toEqual(result);
 });
+
+// test('Рекурсивное сравнение', () => {
+//   const filepath1 = getFixturePath('file1.json');
+//   const filepath2 = getFixturePath('file2.json');
+//   // readFile('file1.yaml');
+//   // readFile('file2.yml');
+//   const result = `{
+//     common: {
+//       + follow: false
+//         setting1: Value 1
+//       - setting2: 200
+//       - setting3: true
+//       + setting3: null
+//       + setting4: blah blah
+//       + setting5: {
+//             key5: value5
+//         }
+//         setting6: {
+//             doge: {
+//               - wow: 
+//               + wow: so much
+//             }
+//             key: value
+//           + ops: vops
+//         }
+//     }
+//     group1: {
+//       - baz: bas
+//       + baz: bars
+//         foo: bar
+//       - nest: {
+//             key: value
+//         }
+//       + nest: str
+//     }
+//   - group2: {
+//         abc: 12345
+//         deep: {
+//             id: 45
+//         }
+//     }
+//   + group3: {
+//         deep: {
+//             id: {
+//                 number: 45
+//             }
+//         }
+//         fee: 100500
+//     }
+// }`;
+
+//   expect(gendiff(filepath1, filepath2)).toEqual(result);
+// });
