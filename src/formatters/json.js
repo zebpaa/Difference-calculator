1c4cb5cc04fb02523ab85c572de7f0d5b74a4871
zebpaa <skvorcov_90@mail.ru>
2024-06-30 14:46:48 +0400

    added stringify function

--- src/formatters/json.js
+++ src/formatters/json.js
@@ -1,3 +1,23 @@
+import _ from 'lodash';
+
+const stringify = (value, replacer = ' ', spacesCount = 1) => {
+  const iter = (currentValue, depth) => {
+    // альтернативный вариант: (typeof currentValue !== 'object' || currentValue === null)
+    if (!_.isObject(currentValue)) {
+      return `${currentValue}`;
+    }
+
+    const indentSize = depth * spacesCount;
+    const currentIndent = replacer.repeat(indentSize);
+    const bracketIndent = replacer.repeat(indentSize - spacesCount);
+    const lines = Object.entries(currentValue).map(([key, val]) => `${currentIndent}${key}: ${iter(val, depth + 1)}`);
+
+    return ['{', ...lines, `${bracketIndent}}`].join('\n');
+  };
+
+  return iter(value, 1);
+};
+
 const formateToJson = (tree) => {
   const result = {};
   // eslint-disable-next-line array-callback-return
@@ -10,7 +30,8 @@ const formateToJson = (tree) => {
       result[`+ ${obj.key}`] = obj.value2;
     } else result[`  ${obj.key}`] = obj.value;
   });
-  return JSON.stringify(result, null, 2);
+  // console.log(result);
+  return stringify(result, ' ', 1);
 };
 
 export default formateToJson;
