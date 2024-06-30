6de0bbcf138d6658186f96c10ca28da500fbfcca
zebpaa <skvorcov_90@mail.ru>
2024-06-30 14:53:38 +0400

    added some comments for debug

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import path from 'path';
 import fs from 'fs';
-import parse from './parse.js';
+import parse from './parsers.js';
 import buildTree from './treeBuilder.js';
 import format from './formatters/json.js';
 
@@ -8,14 +8,18 @@ const fullFilePath = (filepath) => path.resolve(process.cwd(), filepath);
 const extractFormat = (filepath) => path.extname(filepath).slice(1);
 const getData = (filepath) => parse(fs.readFileSync(filepath, 'utf-8'), extractFormat(filepath));
 
-const gendiff = (filepath1, filepath2, formatName) => {
+const gendiff = (filepath1, filepath2) => {
   const fullFilePath1 = fullFilePath(filepath1);
   const fullFilePath2 = fullFilePath(filepath2);
   const data1 = getData(fullFilePath1);
   const data2 = getData(fullFilePath2);
-
+  // console.log('data1: ', data1);
+  // console.log('data2: ', data2);
   const tree = buildTree(data1, data2);
-  const formatedTree = format(tree, formatName);
+  console.log('Tree:', tree);
+  const formatedTree = format(tree);
+  // console.log('##########################');
+  // console.log('Форматированное дерево:', formatedTree);
   return formatedTree;
 };
 
