7d41e018562eeb7246911704ff815f37297fb471
zebpaa <skvorcov_90@mail.ru>
2024-06-30 15:04:39 +0400

    added default option value - stylish

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,7 +7,7 @@ program
   .arguments('<filepath1>, <filepath2>')
   .helpOption('-h, --help', 'output usage information')
   .version('0.0.1')
-  .option('-f, --format [type]', 'output format')
+  .option('-f, --format [type]', 'output format', 'stylish')
   .action((path1, path2) => {
     gendiff(path1, path2, program.opts().format);
   })
