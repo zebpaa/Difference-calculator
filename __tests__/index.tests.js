871fb2614606db8ceeabfc08f791d97159b70408
zebpaa <skvorcov_90@mail.ru>
2024-06-30 15:37:46 +0400

    uncommented 2nd test

--- __tests__/index.tests.js
+++ __tests__/index.tests.js
@@ -8,72 +8,72 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 // const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test('Сравнение плоских файлов (yaml)', () => {
-  const filepath1 = getFixturePath('file1.yaml');
-  const filepath2 = getFixturePath('file2.yml');
-  // readFile('file1.yaml');
-  // readFile('file2.yml');
-  const result = `{
- - follow: false
-   host: hexlet.io
- - proxy: 123.234.53.22
- - timeout: 50
- + timeout: 20
- + verbose: true
-}`;
-
-  expect(gendiff(filepath1, filepath2)).toEqual(result);
-});
-
-// test('Рекурсивное сравнение', () => {
-//   const filepath1 = getFixturePath('file1.json');
-//   const filepath2 = getFixturePath('file2.json');
+// test('Сравнение плоских файлов (yaml)', () => {
+//   const filepath1 = getFixturePath('file1.yaml');
+//   const filepath2 = getFixturePath('file2.yml');
 //   // readFile('file1.yaml');
 //   // readFile('file2.yml');
 //   const result = `{
-//     common: {
-//       + follow: false
-//         setting1: Value 1
-//       - setting2: 200
-//       - setting3: true
-//       + setting3: null
-//       + setting4: blah blah
-//       + setting5: {
-//             key5: value5
-//         }
-//         setting6: {
-//             doge: {
-//               - wow: 
-//               + wow: so much
-//             }
-//             key: value
-//           + ops: vops
-//         }
-//     }
-//     group1: {
-//       - baz: bas
-//       + baz: bars
-//         foo: bar
-//       - nest: {
-//             key: value
-//         }
-//       + nest: str
-//     }
-//   - group2: {
-//         abc: 12345
-//         deep: {
-//             id: 45
-//         }
-//     }
-//   + group3: {
-//         deep: {
-//             id: {
-//                 number: 45
-//             }
-//         }
-//         fee: 100500
-//     }
+//  - follow: false
+//    host: hexlet.io
+//  - proxy: 123.234.53.22
+//  - timeout: 50
+//  + timeout: 20
+//  + verbose: true
 // }`;
 
 //   expect(gendiff(filepath1, filepath2)).toEqual(result);
 // });
+
+test('Рекурсивное сравнение', () => {
+  const filepath1 = getFixturePath('file1.json');
+  const filepath2 = getFixturePath('file2.json');
+  // readFile('file1.yaml');
+  // readFile('file2.yml');
+  const result = `{
+    common: {
+      + follow: false
+        setting1: Value 1
+      - setting2: 200
+      - setting3: true
+      + setting3: null
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+        setting6: {
+            doge: {
+              - wow:
+              + wow: so much
+            }
+            key: value
+          + ops: vops
+        }
+    }
+    group1: {
+      - baz: bas
+      + baz: bars
+        foo: bar
+      - nest: {
+            key: value
+        }
+      + nest: str
+    }
+  - group2: {
+        abc: 12345
+        deep: {
+            id: 45
+        }
+    }
+  + group3: {
+        deep: {
+            id: {
+                number: 45
+            }
+        }
+        fee: 100500
+    }
+}`;
+
+  expect(gendiff(filepath1, filepath2)).toEqual(result);
+});
