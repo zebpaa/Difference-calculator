a2485a0bd03493932882a3ec4cdda52bbb3a6829
zebpaa <skvorcov_90@mail.ru>
2024-07-07 15:48:03 +0400

    uncommented the tests

--- __tests__/index.tests.js
+++ __tests__/index.tests.js
@@ -77,25 +77,25 @@ test('Рекурсивное сравнение (из проекта)', () => {
   expect(gendiff(filepath1, filepath2)).toEqual(result);
 });
 
-// test('Плоский формат', () => {
-//   const filepath1 = getFixturePath('file1.json');
-//   const filepath2 = getFixturePath('file2.json');
-//   // readFile('file1.yaml');
-//   // readFile('file2.yml');
-//   const result = `Property 'common.follow' was added with value: false
-// Property 'common.setting2' was removed
-// Property 'common.setting3' was updated. From true to null
-// Property 'common.setting4' was added with value: 'blah blah'
-// Property 'common.setting5' was added with value: [complex value]
-// Property 'common.setting6.doge.wow' was updated. From '' to 'so much'
-// Property 'common.setting6.ops' was added with value: 'vops'
-// Property 'group1.baz' was updated. From 'bas' to 'bars'
-// Property 'group1.nest' was updated. From [complex value] to 'str'
-// Property 'group2' was removed
-// Property 'group3' was added with value: [complex value]`;
+test('Плоский формат', () => {
+  const filepath1 = getFixturePath('file1.json');
+  const filepath2 = getFixturePath('file2.json');
+  // readFile('file1.yaml');
+  // readFile('file2.yml');
+  const result = `Property 'common.follow' was added with value: false
+Property 'common.setting2' was removed
+Property 'common.setting3' was updated. From true to null
+Property 'common.setting4' was added with value: 'blah blah'
+Property 'common.setting5' was added with value: [complex value]
+Property 'common.setting6.doge.wow' was updated. From '' to 'so much'
+Property 'common.setting6.ops' was added with value: 'vops'
+Property 'group1.baz' was updated. From 'bas' to 'bars'
+Property 'group1.nest' was updated. From [complex value] to 'str'
+Property 'group2' was removed
+Property 'group3' was added with value: [complex value]`;
 
-//   expect(gendiff(filepath1, filepath2, 'plain')).toEqual(result);
-// });
+  expect(gendiff(filepath1, filepath2, 'plain')).toEqual(result);
+});
 
 // test('Вывод в json', () => {
 //   const filepath1 = getFixturePath('file1.json');
