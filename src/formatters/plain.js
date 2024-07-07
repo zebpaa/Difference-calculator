5c204cae8cf9197fef6430913bfdf2a7a39ebdfb
zebpaa <skvorcov_90@mail.ru>
2024-07-07 15:48:38 +0400

    fixed plain.js

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,13 +1,34 @@
+import _ from 'lodash';
+
+const addQuotes = (value) => (_.isString(value) ? `'${value}'` : value);
+
+const getPathOfElement = (acc) => {
+  if (acc !== '') {
+    const newAcc = `${acc}`;
+    return newAcc;
+  }
+  return '';
+};
+
+const isObject = (value) => {
+  if (_.isObject(value)) {
+    return '[complex value]';
+  }
+  return `${addQuotes(value)}`;
+};
+
 const formateToPlain = (tree) => {
-  const formatedTree = tree.flatMap((obj) => {
-    if (obj.type === 'nested') formateToPlain(obj.children);
-    if (obj.type === 'added') return `Property '${obj.key}' was ${obj.type} with value: ${obj.value}`;
-    if (obj.type === 'deleted') return `Property '${obj.key}' was removed`;
-    if (obj.type === 'changed') return `Property '${obj.key}' was updated. From ${obj.value1} to ${obj.value2}`;
+  const iter = (node, acc) => node.flatMap((obj) => {
+    if (obj.type === 'nested') {
+      const newAcc = `${acc}${obj.key}.`;
+      return `${iter(obj.children, newAcc)}`;
+    }
+    if (obj.type === 'added') return `Property '${getPathOfElement(acc)}${obj.key}' was added with value: ${isObject(obj.value)}`;
+    if (obj.type === 'deleted') return `Property '${getPathOfElement(acc)}${obj.key}' was removed`;
+    if (obj.type === 'changed') return `Property '${getPathOfElement(acc)}${obj.key}' was updated. From ${isObject(obj.value1)} to ${isObject(obj.value2)}`;
     return [];
-  });
-  const result = formatedTree.map((obj) => obj);
-  return result.join('\n');
+  }).join('\n');
+  return iter(tree, '');
 };
 
 export default formateToPlain;
