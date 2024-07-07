184f6a22249df5259319371881643436da55a1c5
zebpaa <skvorcov_90@mail.ru>
2024-07-07 16:29:47 +0400

    fixed tests and fixtures

--- __tests__/index.tests.js
+++ __tests__/index.tests.js
@@ -1,31 +1,63 @@
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
-// import fs from 'fs';
 import gendiff from '../index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-// const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test('Сравнение плоских файлов (yaml)', () => {
+test('Рекурсивное сравнение yml файлов', () => {
   const filepath1 = getFixturePath('file1.yaml');
   const filepath2 = getFixturePath('file2.yml');
-  // readFile('file1.yaml');
-  // readFile('file2.yml');
   const result = `{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
+    common: {
+      + follow: false
+        setting1: Value 1
+      - setting2: 200
+      - setting3: true
+      + setting3: null
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+        setting6: {
+            doge: {
+              - wow: 
+              + wow: so much
+            }
+            key: value
+          + ops: vops
+        }
+    }
+    group1: {
+      - baz: bas
+      + baz: bars
+        foo: bar
+      - nest: {
+            key: value
+        }
+      + nest: str
+    }
+  - group2: {
+        abc: 12345
+        deep: {
+            id: 45
+        }
+    }
+  + group3: {
+        deep: {
+            id: {
+                number: 45
+            }
+        }
+        fee: 100500
+    }
 }`;
 
-  expect(gendiff(filepath1, filepath2)).toEqual(result);
+  expect(gendiff(filepath1, filepath2, 'stylish')).toEqual(result);
 });
 
-test('Рекурсивное сравнение (из проекта)', () => {
+test('Рекурсивное сравнение json файлов', () => {
   const filepath1 = getFixturePath('file1.json');
   const filepath2 = getFixturePath('file2.json');
 
@@ -80,8 +112,6 @@ test('Рекурсивное сравнение (из проекта)', () => {
 test('Плоский формат', () => {
   const filepath1 = getFixturePath('file1.json');
   const filepath2 = getFixturePath('file2.json');
-  // readFile('file1.yaml');
-  // readFile('file2.yml');
   const result = `Property 'common.follow' was added with value: false
 Property 'common.setting2' was removed
 Property 'common.setting3' was updated. From true to null
