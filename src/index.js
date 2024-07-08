21f4cde52388fe1f11d154f5c2b1af9e9619945c
zebpaa <skvorcov_90@mail.ru>
2024-07-08 21:05:05 +0400

    deleted default value for param formatName

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const fullFilePath = (filepath) => path.resolve(process.cwd(), filepath);
 const extractFormat = (filepath) => path.extname(filepath).slice(1);
 const getData = (filepath) => parse(fs.readFileSync(filepath, 'utf-8'), extractFormat(filepath));
 
-const gendiff = (filepath1, filepath2, formatName = 'stylish') => {
+const gendiff = (filepath1, filepath2, formatName) => {
   const fullFilePath1 = fullFilePath(filepath1);
   const fullFilePath2 = fullFilePath(filepath2);
   const data1 = getData(fullFilePath1);
